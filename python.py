c336c13640cd301339db51ae7e2f7cbe8d093696
LuckyShmucky <christianluciano567@gmail.com>
2022-06-21 11:38:56 -0700

    features/ find new solutions for max_num and num_within working/ still working on pascal

--- python.py
+++ python.py
@@ -1,15 +1,13 @@
-from operator import truediv
-
-
 def max_num(*nums):
-    maximum_num = 0
+    maximum_num = nums[0]
     for i in nums:
         if i > maximum_num:
             maximum_num = i
             # print
     print(maximum_num)
 
-# max_num(2,32,-5)
+
+# max_num(-12, -5, -3, -2, 1, 100)
 
 
 def mult_list(nums):
@@ -33,15 +31,21 @@ def rev_string(string):
 # print(rev_string('kayaks'))
 
 def num_within(number, beginning, end):
-    num_is_in_range = None
-    if number >= beginning and number <= end:
-        num_is_in_range = True
-    else:
-        num_is_in_range = False
-    return num_is_in_range
+
+    return number >= beginning and number <= end
 
 
-# print(num_within(2, 1, 3))
+# print(num_within(0, 1, 3))
 
-def pascal():
-    triangle_rows = [[1],[1,1]]
+
+def pascal(n):
+    triangle_rows = []
+# if we have an input of greater number than the length of rows we already have, we want to add it to list of rows in the triangle
+    new_row = [1]
+    for num in range(1, n+1):
+        for n in range(1, num):
+            new_row.append([1])
+        print(new_row)
+
+
+pascal(3)
